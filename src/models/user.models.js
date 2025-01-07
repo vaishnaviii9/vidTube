f8227582906072e6f497813beefb59f284a1239a
Vaishnavi <vaishnavitonpe0@gmail.com>
2025-01-07 13:55:23 +0530

    add .npmignore files and various bin scripts for dependencies

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -11,7 +11,9 @@
 // updatedAt Date 
 
 import mongoose, {Schema} from "mongoose";
- 
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
 const userSchema = new Schema(
     {
           username : {
@@ -60,5 +62,50 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
+
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) {
+    return next()
+  }
+
+  this.password = bcrypt.hash(this.password, 10)
+
+  next()
+})
+
+userSchema.methods.isPasswordCorrect  = async function (password) {
+ 
+  return await bcrypt.compare(password,this.password)
+
+}
+
+userSchema.methods.generateAccessToken = function(){
+  //short lived access token
+  return jwt.sign({
+   _id : this._id,
+   email: this.email,
+   username: this.username,
+   fullname : this.fullName
+  },
+process.env.ACCESS_TOKEN_SECRET,
+{
+  expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+}
+)
+}
+
+
+userSchema.methods.generateRefreshToken = function(){
+  //short lived access token
+  return jwt.sign({
+   _id : this._id
+  },
+process.env.REFRESH_TOKEN_SECRET,
+{
+  expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+}
+)
+}
+
 
 export const User = mongoose.model("User",userSchema)
