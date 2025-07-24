1010b148e0cafc5e37b9a8afc88a3f6594deba97
Vaishnavi <vaishnavitonpe0@gmail.com>
2025-07-24 07:55:59 -0400

    refactor: reorganize user routes for improved readability and structure

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,25 @@
 import { Router } from "express";
-import { registerUser,logoutUser } from "../controllers/user.controller.js";
+import { 
+    registerUser,
+    logoutUser, 
+    loginUser, 
+    refreshAccessToken, 
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    updateAccountDetails,
+    updateUserCoverImage,
+    getWatchHistory
+} from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middlewares.js';
 import {verifyJWT} from '../middlewares/auth.middlewares.js'
 
 
 const router = Router()
 
+//UNSECURED ROUTES
+
+//register user
 router.route("/register").post(
     upload.fields([
         { 
@@ -20,14 +34,61 @@ router.route("/register").post(
     registerUser
 )
 
+//user login
+router.route("/login").post(loginUser)
+
+//refreshaccesstoken
+router.route("/refresh-token").post(refreshAccessToken)
+
+//SECURED ROUTES
 
 //user logout
-//secured routes
-
 router.route("/logout").post(
     verifyJWT,
     // something,
     logoutUser
+)
+
+//change password
+router.route("/change-password").post(
+    verifyJWT,
+    changeCurrentPassword
+)
+
+//get current user
+router.route("/current-user").get(
+    verifyJWT,
+    getCurrentUser
+)
+
+//get user channel profile
+router.route("/c/:username").get(
+    verifyJWT,
+    getUserChannelProfile
+)
+
+//update account details
+router.route("/update-account").patch(
+    verifyJWT,
+    updateAccountDetails
+)
+
+//update user avatar and cover image
+router.route("/avatar").patch(
+    verifyJWT,
+    upload.single("avatar"),
+    updateUserAvatar
+)
+router.route("/cover-image").patch(
+    verifyJWT,
+    upload.single("coverImage"),
+    updateUserCoverImage
+)
+
+//get watch history
+router.route("/history").get(
+    verifyJWT,
+    getWatchHistory
 )
 
 export default router
